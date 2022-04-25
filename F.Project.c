15874475281915d11b6c3fb3877a3ee7f039b4db
Roshan Khaling <roshanraee10github@gmail.com>
2022-04-25 08:01:18 +0545

    First Day WELCOOME NOTE

---
+++ F.Project.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int i;
+	printf("\n\t\t\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\n");
+	printf("\t\t\xdb                                               \xdb\n");
+	printf("\t\t\xdb          =============================        \xdb\n");
+	printf("\t\t\xdb          STUDENT DATABASE MANAGEMENT          \xdb\n");
+	printf("\t\t\xdb                   SYSTEM                      \xdb\n");
+	printf("\t\t\xdb          =============================        \xdb\n");
+	printf("\t\t\xdb                CITIZEN COLLEGE                \xdb\n");
+	printf("\t\t\xdb              Kumaripati,Lalitpur              \xdb\n");
+	printf("\t\t\xdb                                               \xdb\n");
+	printf("\t\t\xdb                                               \xdb\n");
+	printf("\t\t\xdb         CONTACT US:98765432100,0610423280     \xdb\n");
+	printf("\t\t\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\n\n\n");
+
+    for(i=0;i<80;i++)
+		printf("-");
+    printf("                 \n   \t \t \t \t  ENTER ANY KEY TO CONTINUE . . . ");
+    printf("\n");
+	for(i=0;i<80;i++)
+		printf("-");
+	getch();
+	return 0;	
+}
